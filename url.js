34875ace356078b6e3c071adc5dea01cd29cce42
李宏吉 <lihongji@lihongjideMacBook-Pro.local>
2016-03-26 08:30:42 +0800

    add

--- url.js
+++ url.js
@@ -6,5 +6,8 @@ var url =require("url");
 var querystring=require('querystring');
 var u=url.parse('https://www.npmjs.com/package/url?a=1&b=2');
 
+var a=querystring.parse(u.query);
 
-console.log(querystring.parse(u.query));
+console.log(a);
+
+console.log(querystring.stringify(a));
