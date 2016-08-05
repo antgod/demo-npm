341b7b5f54efb0a9ca7ae3577530af37e2e60d76
李宏吉 <lihongji@meituan.com>
2016-08-05 15:33:48 +0800

    add

---
+++ cookie.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lihongji on 16/8/5.
+ */
+
+var Cookie=require('tiny-cookie');
+
+//var now = new Date;
+//now.setMonth(now.getMonth() + 1);
+//
+//Cookie.set('foo', 'Foo', { expires: now.toGMTString() });
